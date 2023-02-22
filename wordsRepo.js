0eebb10013547db167e2b2fe6b7740bbb4c25b5c
drew-sarette <103331340+drew-sarette@users.noreply.github.com>
2023-02-22 15:11:39 +0000

    Update api requests

--- wordsRepo.js
+++ wordsRepo.js
@@ -7,9 +7,9 @@ const wordsRepo = {
     console.log(`Checking DB for ${word}`);
     const keys = await db.list();
     // uncomment to empty database
-    for (let key of keys) {
-      db.delete(key).then(() => { });
-    }
+    // for (let key of keys) {
+    //   db.delete(key).then(() => { });
+    // }
     let entry = null;
     for (let key of keys) {
       console.log(key);
@@ -21,40 +21,41 @@ const wordsRepo = {
   },
 
   checkWordsAPI: async function getWordData(word) {
-    try {
-      console.log(`Checking WordsAPI for ${word}`);
-
-      const options = {
-        method: 'GET',
-        url: `https://wordsapiv1.p.rapidapi.com/words/${word}/synonyms`,
-        headers: {
-          'X-RapidAPI-Key': process.env.WORDSAPI_KEY,
-          'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
-        }
-      };
-      const synonymRes = await axios.request(options);
-
-      options.url = `https://wordsapiv1.p.rapidapi.com/words/${word}/syllables`;
-      const syllableRes = await axios.request(options);
-
-      if (synonymRes.status !== 200 || syllableRes.status !== 200) {
-        throw new Error(`Failed to get ${word} data from WordsAPI.`);
+    console.log(`Checking WordsAPI for ${word}`);
+  
+    const options = {
+      method: 'GET',
+      url: `https://wordsapiv1.p.rapidapi.com/words/${word}/syllables`,
+      headers: {
+        'X-RapidAPI-Key': process.env.WORDSAPI_KEY,
+        'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
       }
-
-      const listOfSynonyms = [];
-      synonymRes.data.synonyms.filter(w => !(w.includes(" "))).forEach(w => listOfSynonyms.push(w));
-      const numSyllables = syllableRes.data.syllables.count;
-      const newEntry = {
-        "word": word,
-        "syllables": numSyllables,
-        "synonyms": listOfSynonyms,
-      };
-      return newEntry;
+    };
+  
+    let numSyllables = null;
+    let listOfSynonyms = [];
+  
+    try {
+      const syllableRes = await axios.request(options);
+      numSyllables = syllableRes.data.syllables.count;
+    } catch (err) {
+      console.log("Failed to get syllables from WordsAPI", err.msg);
+      numSyllables = 1;
     }
-    catch (error) {
-      console.error("Error occurred while getting word data: ", error.message);
-      return;
+  
+    options.url = `https://wordsapiv1.p.rapidapi.com/words/${word}/synonyms`;
+  
+    try {
+      const synonymRes = await axios.request(options);
+      listOfSynonyms = synonymRes.data.synonyms.filter(w => !(w.includes(" ")));
+    } catch (err) {
+      console.log("Failed to get synonyms from WordsAPI", err.msg);
     }
+    return {
+      "word": word,
+      "syllables": numSyllables,
+      "synonyms": listOfSynonyms,
+    };
   },
 
   addToDB: async function(entry) {
